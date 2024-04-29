5cd218eb1bb7f4deb59127767e0e57b9033caf23
Armond-Guze <Armond.Guze@yahoo.com>
2024-04-29 15:02:09 -0400

    fixed navbar

--- job-portfolio/src/components/Navbar.jsx
+++ job-portfolio/src/components/Navbar.jsx
@@ -17,14 +17,21 @@ const Navbar = () => {
       {/* menu */}
 
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <Link to='about' smooth={true} duration={500}>
+        <li><Link to='home' smooth={true} duration={500}>
+            Home
+          </Link></li>
+        <li><Link to='about' smooth={true} duration={500}>
             About
-          </Link>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+          </Link></li>
+        <li><Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link></li>
+        <li><Link to='work' smooth={true} duration={500}>
+            Work
+          </Link></li>
+        <li><Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link></li>
       </ul>
 
       {/* Hamburger */}
