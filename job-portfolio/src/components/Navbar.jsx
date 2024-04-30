4500c6ef3bea57395bf6f7086d17ef66d2f8b192
Armond-Guze <Armond.Guze@yahoo.com>
2024-04-29 21:26:13 -0400

    fixed react scroll

--- job-portfolio/src/components/Navbar.jsx
+++ job-portfolio/src/components/Navbar.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
-import Logo from "../assets/logo1.png";
+import Logo from "../assets/ag logo.png";
 import { TfiMenuAlt } from "react-icons/tfi";
 import { MdCancel } from "react-icons/md";
-import { FaLinkedin, FaGithubSquare, FaBook  } from "react-icons/fa";
+import { FaLinkedin, FaGithubSquare, FaBook } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleclick = () => setNav(!nav);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="logo Image" style={{ width: "50px" }} />
+        <img src={Logo} alt="logo Image" style={{ width: "400px" }} className="bg-opacity-50" />
       </div>
 
       {/* menu */}
 
       <ul className="hidden md:flex">
-        <li><Link to='home' smooth={true} duration={500}>
+        <li>
+          <Link to="home" smooth={true} duration={500}>
             Home
-          </Link></li>
-        <li><Link to='about' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
             About
-          </Link></li>
-        <li><Link to='skills' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
             Skills
-          </Link></li>
-        <li><Link to='work' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
             Work
-          </Link></li>
-        <li><Link to='contact' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger */}
@@ -47,39 +57,68 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl"><Link onClick={handleclick} to='home' smooth={true} duration={500}>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleclick} to="home" smooth={true} duration={500}>
             Home
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleclick} to='about' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleclick} to="about" smooth={true} duration={500}>
             About
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleclick} to='skills' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleclick} to="skills" smooth={true} duration={500}>
             Skills
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleclick} to='work' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleclick} to="work" smooth={true} duration={500}>
             Work
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleclick} to='contact' smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleclick} to="contact" smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-            <a className="flex justify-between items-center w-full text-gray-300" href="/">LinkedIn <FaLinkedin size={30}/></a>
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
-            <a className="flex justify-between items-center w-full text-gray-300" href="/">Github <FaGithubSquare size={30}/></a>
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Github <FaGithubSquare size={30} />
+            </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
-            <a className="flex justify-between items-center w-full text-gray-300" href="/">Email <MdEmail size={30}/></a>
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Email <MdEmail size={30} />
+            </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#808080]">
-            <a className="flex justify-between items-center w-full text-gray-300" href="/">Resume <FaBook  size={30}/></a>
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <FaBook size={30} />
+            </a>
           </li>
-          
         </ul>
       </div>
     </div>
